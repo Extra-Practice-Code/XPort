9069d5874ecbdac060ac2f831baa4a5bdd347072
Gijs de Heij <gijs@de-heij.com>
2018-09-26 17:27:07 +0200

    Forgot url change

--- ethertoff/urls.py
+++ ethertoff/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
         {'template_name': 'login.html'}, name='login'),
     path('accounts/logout', auth_views.LogoutView.as_view(),
         {'template_name': 'logout.html'}, name='logout'),
+    path('accounts/password_change', auth_views.PasswordChangeView.as_view(), name="password_change"),
     path('create/', views.padCreate, name='pad-create'),
     re_path(r'(?P<mode>[r|s|p])/(?P<slug>[^/]+)$', views.pad_read, name='pad-read'),
     #re_path(r'r/(?P<slug>[^/]+)$', views.pad, name='pad-read'),
