9ded7d5af7c3b45615d25249f220c87f14742b8d
Gijs de Heij <gijs@de-heij.com>
2018-09-26 17:17:41 +0200

    Added password change

--- ethertoff/settings.py
+++ ethertoff/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ethertoff',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'etherpadlite',
-    'ethertoff',
     #'south'
 ]
 
