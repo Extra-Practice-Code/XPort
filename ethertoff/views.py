540f3f80740a6e2f6feb00b437241f0552bfc2e9
Gijs de Heij <gijs@de-heij.com>
2018-10-19 17:44:25 +0200

    Use ToC extension instead of deprecated headerid

--- ethertoff/views.py
+++ ethertoff/views.py
@@ -349,7 +349,7 @@ def pad_read(request, mode="r", slug=None):
         # we don’t want Etherpads automatically generated HTML, we want plain text.
         text = epclient.getText(padID)['text']
         if extension in ['.md', '.markdown']:
-            md = markdown.Markdown(extensions=['extra', 'meta', 'headerid(level=2)', 'attr_list', 'figcaption'])
+            md = markdown.Markdown(extensions=['extra', 'meta', 'TocExtension(baselevel=2)', 'attr_list', 'figcaption'])
             text = md.convert(text)
             try:
                 meta = md.Meta
