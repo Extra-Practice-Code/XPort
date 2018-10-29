e3caff5a1ab36387bef441c0294a9d56f2f5af4b
Gijs de Heij <gijs@de-heij.com>
2018-10-29 17:22:51 +0100

    Mmm, better to order on this?

--- ethertoff/views.py
+++ ethertoff/views.py
@@ -475,9 +475,9 @@ def publish(request):
 def manage(request, path=[]):
     if len(path) > 0:
         path = path.split('/')
-        pads = Pad.objects.filter(display_slug__startswith='::'.join(path) + '::').order_by('name')
+        pads = Pad.objects.filter(display_slug__startswith='::'.join(path) + '::').order_by('display_slug')
     else:
-        pads = Pad.objects.all().order_by('name')
+        pads = Pad.objects.all().order_by('display_slug')
     # paginator = Paginator(pads, PADS_PER_PAGE)
 
     tree = makeLeaf()
