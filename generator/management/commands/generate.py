3d6dcc4471ddc7da6b5b56eb39cb69b2ca86ced0
Gijs <gijs@de-heij.com>
2022-08-17 16:50:43 +0200

    Force gif file type on dithered images.

--- generator/management/commands/generate.py
+++ generator/management/commands/generate.py
@@ -24,6 +24,68 @@ FIELD_DATE_FORMAT = '%d.%m.%Y'
 FIELD_DATETIME_FORMAT = '%d-%m-%Y %H:%M'
 FIELD_TIME_FORMAT = '%H:%M'
 
+"""
+
+    Monkey patching the ThumbnailerMixin to force gif images on dither as the mixin
+    prevents to use a custom namer: https://github.com/django-cms/django-filer/issues/1093
+
+"""
+
+from filer.utils.filer_easy_thumbnails import ThumbnailerNameMixin
+
+def patched_get_thumbnail_name(self, thumbnail_options, transparent=False):
+    print('Patched thumbnailer')
+    print(thumbnail_options)
+    """
+    A version of ``Thumbnailer.get_thumbnail_name`` that produces a
+    reproducible thumbnail name that can be converted back to the original
+    filename.
+    """
+    path, source_filename = os.path.split(self.name)
+    source_extension = os.path.splitext(source_filename)[1][1:].lower()
+    preserve_extensions = self.thumbnail_preserve_extensions
+    if 'dither' in thumbnail_options:
+      # Force gif when dither is activated.
+      extension = 'gif'
+    elif preserve_extensions is True or source_extension == 'svg' or \
+            isinstance(preserve_extensions, (list, tuple)) and source_extension in preserve_extensions:
+        extension = source_extension
+    elif transparent:
+        extension = self.thumbnail_transparency_extension
+    else:
+        extension = self.thumbnail_extension
+    extension = extension or 'jpg'
+
+    thumbnail_options = thumbnail_options.copy()
+    size = tuple(thumbnail_options.pop('size'))
+    initial_opts = ['{0}x{1}'.format(*size)]
+    quality = thumbnail_options.pop('quality', self.thumbnail_quality)
+    if extension == 'jpg':
+        initial_opts.append('q{}'.format(quality))
+    elif extension == 'svg':
+        thumbnail_options.pop('subsampling', None)
+        thumbnail_options.pop('upscale', None)
+
+    opts = list(thumbnail_options.items())
+    opts.sort()   # Sort the options so the file name is consistent.
+    opts = ['{}'.format(v is not True and '{}-{}'.format(k, v) or k)
+            for k, v in opts if v]
+    all_opts = '_'.join(initial_opts + opts)
+
+    basedir = self.thumbnail_basedir
+    subdir = self.thumbnail_subdir
+
+    # make sure our magic delimiter is not used in all_opts
+    all_opts = all_opts.replace('__', '_')
+    filename = '{}__{}.{}'.format(source_filename, all_opts, extension)
+
+    return os.path.join(basedir, path, subdir, filename)
+
+
+ThumbnailerNameMixin.get_thumbnail_name = patched_get_thumbnail_name
+
+
+
 
 from generator.settings import DATE_OUTPUT_FORMAT
 
