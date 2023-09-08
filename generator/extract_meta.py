1d4d0e49d9b85ae8dcb7ae329d95391ee8b6ab4b
Gijs <gijs@de-heij.com>
2023-09-08 10:26:54 +0200

    Better meta extraction.

--- generator/extract_meta.py
+++ generator/extract_meta.py
@@ -7,6 +7,42 @@ META_MORE_RE = re.compile(r'^[ ]{4,}(?P<value>.*)')
 BEGIN_RE = re.compile(r'^-{3}(\s.*)?')
 END_RE = re.compile(r'^(-{3}|\.{3})(\s.*)?')
 
+EMPTY_LINE_RE = re.compile(r'^\s*$')
+ESCAPED_UNDERSCORES = re.compile(r'(?<!\\)\\_')
+
+def filter_empty_lines_markdown_export (lines):
+  print(lines)
+  while EMPTY_LINE_RE.match(lines[0]):
+    lines.pop(0)
+
+  i = 0
+  lines_to_delete = []
+
+  while i < len(lines):
+    lines[i] = ESCAPED_UNDERSCORES.subn('_', lines[i])[0]
+    line = lines[i]
+    next_line = lines[i+1]
+
+    if '\\' in line:
+       print('*****\n', line, ESCAPED_UNDERSCORES.subn('_', lines[i])[0])
+
+
+    if META_RE.match(line) or (i > 0 and META_RE.match(line)):
+      if EMPTY_LINE_RE.match(next_line):
+        lines_to_delete.append(i+1)
+        i += 2
+      else:
+        i += 1
+    else:
+      break
+
+
+  for line_to_delete in lines_to_delete[::-1]:
+    lines.pop(line_to_delete)
+
+  return lines
+
+
 """
     Taken from the python markdown extension
 """
@@ -15,7 +51,8 @@ def extract_meta (content):
     """ Extract and parse metadata from the file. """
     meta = OrderedDict()
     key = None
-    lines = content.split('\n')
+    lines = filter_empty_lines_markdown_export(content.split('\n'))
+    print(lines)
     if lines and BEGIN_RE.match(lines[0]):
         lines.pop(0)
     while lines:
