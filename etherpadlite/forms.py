5fffce479a12e330dc43cc335501d495d7c9cd72
Gijs <gijs@de-heij.com>
2024-10-23 19:41:08 +0200

    Change redirect after pad name change. Make folder not required on pad creation.

--- etherpadlite/forms.py
+++ etherpadlite/forms.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class PadCreate(forms.Form):
     name = forms.CharField(label=_("Name"))
-    folder = forms.CharField(label=_("Folder"))
+    folder = forms.CharField(label=_("Folder"), required=False)
     group = forms.CharField(widget=forms.HiddenInput)
 
 
--- ethertoff/views.py
+++ ethertoff/views.py
@@ -345,7 +345,7 @@ def padRename(request, pad):
 
             if folder:
                 # path = path.replace(settings.PAD_NAMESPACE_SEPARATOR, '/')
-                return redirect('manage', path_string=folder)
+                return redirect('manage', args=[folder])
             else:
                 return redirect('manage')
 
